34cc53edb244183465a811319ed87884417ab502
GrantGastin <103415118+GrantGastin@users.noreply.github.com>
2022-06-20 12:06:59 +0300

    ManagerFilm

---
+++ src/main/java/Poster/PosterItems.java
@@ -0,0 +1,16 @@
+package Poster;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+
+public class PosterItems {
+    private int id;
+    private String filmName;
+    private String genre;
+
+}
